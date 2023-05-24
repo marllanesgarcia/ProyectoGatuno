0dd950478ade0e0ebc0f580dcc0fd514991fa265
marllanesgarcia <sakiro1999@gmail.com>
2023-05-24 16:17:46 +0200

    v

--- javascript.js
+++ javascript.js
@@ -9,6 +9,19 @@ var link = "317webpingu@gmail.com"
 window.location.href = "mailto:" +link;
 }
 
-function redireccionar(url) {
-    window.location.href = url;
+  function cargarXML() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        mostrarXML(this);
+      }
+    };
+    xhttp.open("GET", "https://marllanesgarcia.github.io/ProyectoGatuno/necesidadesFelinas.xml", true); // Reemplaza "ruta/archivo.xml" con la ruta real de tu archivo XML
+    xhttp.send();
+  }
+
+  function mostrarXML(xml) {
+    var xmlDoc = xml.responseXML;
+    var xmlString = new XMLSerializer().serializeToString(xmlDoc);
+    document.getElementById("xmlDiv").innerHTML = "<pre><code>" + xmlString + "</code></pre>";
   }
